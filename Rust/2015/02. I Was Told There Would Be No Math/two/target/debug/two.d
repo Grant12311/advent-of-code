db8aebccd6c366b6be5f3549b66592dfb2b93a2f
Grant Joseph <granetejo@gmail.com>
2023-11-04 00:31:05 -0400

    Ignore "target/" folders.

--- Rust/2015/02. I Was Told There Would Be No Math/two/target/debug/two.d
+++
@@ -1,1 +0,0 @@
-/home/grant/Advent\ of\ Code/2015/02.\ I\ Was\ Told\ There\ Would\ Be\ No\ Math/two/target/debug/two: /home/grant/Advent\ of\ Code/2015/02.\ I\ Was\ Told\ There\ Would\ Be\ No\ Math/two/src/main.rs
